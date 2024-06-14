68ab22a0a7ce9f5cb0746dc3dfa89cd1ba35af7a
generyand <generyan.dep@gmail.com>
2024-06-14 11:59:04 +0800

    Minor changes in animation.

--- src/components/RandomQuoteGenerator.jsx
+++ src/components/RandomQuoteGenerator.jsx
@@ -26,15 +26,26 @@ export default function RandomQuoteGenerator() {
   }, []);
   return (
     <div className="text-white mt-[15vh] max-w-2xl flex flex-col items-center mx-4">
-      <h1 className="text-2xl font-bold text-center text-transparent lg:text-4xl bg-gradient-to-r to-pink-600 from-orange-800 bg-clip-text ">
+      <motion.h1 
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 , delay: 0.1}}
+      className="text-2xl font-bold text-center text-transparent lg:text-4xl bg-gradient-to-r to-pink-600 from-orange-800 bg-clip-text ">
         Random Famous Quotes
-      </h1>
-      <p className="mb-8 text-center text-transparent bg-gradient-to-r to-pink-600 from-orange-800 bg-clip-text ">
+      </motion.h1>
+      <motion.p 
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 , delay: 0.2}}
+      className="mb-8 text-center text-transparent bg-gradient-to-r to-pink-600 from-orange-800 bg-clip-text ">
         Discover random wisdom from various authors and genres. Enjoy a new
         perspective with each quote!
-      </p>
+      </motion.p>
 
-      <div className="card | mb-12 p-8 shadow-xl rounded-md min-w-full">
+      <motion.div 
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="card | mb-12 p-8 shadow-xl rounded-md min-w-full">
         <div className="text">
           {/* Quote */}
           <p className="mb-4 text-xl">
@@ -44,7 +55,7 @@ export default function RandomQuoteGenerator() {
           {/* Author */}
           <p className="text-xl text-end">— {author ? author : "Author"}</p>
         </div>
-      </div>
+      </motion.div>
 
       <button
         className="px-4 py-2 bg-orange-400 rounded-md active:bg-orange-300 hover:bg-orange-500"
