5e946764ef5455c4c4bcb6580880ddc06c054228
generyand <generyan.dep@gmail.com>
2024-06-14 08:17:50 +0800

    minor change

--- src/RandomQuoteGenerator.jsx
+++ src/RandomQuoteGenerator.jsx
@@ -23,27 +23,27 @@ export default function RandomQuoteGenerator() {
   }, []);
   return (
     <div className="text-white mt-[15vh] max-w-2xl flex flex-col items-center mx-4">
-      <h1 className="text-2xl lg:text-4xl font-bold  text-center bg-gradient-to-r to-pink-600 from-orange-800 text-transparent bg-clip-text ">
+      <h1 className="text-2xl font-bold text-center text-transparent lg:text-4xl bg-gradient-to-r to-pink-600 from-orange-800 bg-clip-text ">
         Random Famous Quotes
       </h1>
-      <p className="mb-8 text-center bg-gradient-to-r to-pink-600 from-orange-800 text-transparent bg-clip-text ">
+      <p className="mb-8 text-center text-transparent bg-gradient-to-r to-pink-600 from-orange-800 bg-clip-text ">
         Discover random wisdom from various authors and genres. Enjoy a new
         perspective with each quote!
       </p>
 
       <div className="card | mb-12 p-8 shadow-xl rounded-md min-w-full">
         <div className="text">
-          <p className="text-xl text-balance mb-4">
+          <p className="mb-4 text-xl">
             {quote
               ? `"${quote}"`
               : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque mollitia asperiores optio possimus voluptatum rerum ipsa distinctio! Harum dicta voluptatibus deserunt adipisci numquam ex ipsum!"}
           </p>
-          <p className=" text-end text-xl">— {author ? author : "Anonymous"}</p>
+          <p className="text-xl  text-end">— {author ? author : "Anonymous"}</p>
         </div>
       </div>
 
       <button
-        className="rounded-md active:bg-orange-300 hover:bg-orange-500 bg-orange-400 px-4 py-2"
+        className="px-4 py-2 bg-orange-400 rounded-md active:bg-orange-300 hover:bg-orange-500"
         onClick={getQuote}
       >
         Get Quote
