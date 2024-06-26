5438ce50ed8c04d2dbc6f8c11f5469536468cfe9
generyand <generyan.dep@gmail.com>
2024-06-26 14:54:15 +0800

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import RandomQuoteGenerator from "./components/RandomQuoteGenerator";
 import backgroundImage from "./assets/background.webp";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <div
-      className="flex justify-center h-full mx-auto bg-center bg-no-repeat bg-cover"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-      id="quote-generator"
-    >
-      <RandomQuoteGenerator />
-    </div>
+    <>
+      <Analytics />
+      <div
+        className="flex justify-center h-full mx-auto bg-center bg-no-repeat bg-cover"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+        id="quote-generator"
+      >
+        <RandomQuoteGenerator />
+      </div>
+    </>
   );
 }
 
